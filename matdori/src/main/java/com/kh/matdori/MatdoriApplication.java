9b1ff31cdf92b5dcb304a81808de017e0459a33f
jaenyyyy <wodms0419@naver.com>
2023-10-17 19:47:45 +0900

    matdori 패키지 생성

--- matdori/src/main/java/com/kh/matdori/MatdoriApplication.java
+++ matdori/src/main/java/com/kh/matdori/MatdoriApplication.java
@@ -1,4 +1,4 @@
-package matdori;
+package com.kh.matdori;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- matdori/src/main/java/com/kh/matdori/ServletInitializer.java
+++ matdori/src/main/java/com/kh/matdori/ServletInitializer.java
@@ -1,4 +1,4 @@
-package matdori;
+package com.kh.matdori;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
