e9db146fb60c9356a6253f50f9424d1fb9a79cb2
MJH098 <narutoye@naver.com>
2023-11-08 11:22:15 +0900

    business insert 확인

--- matdori/src/main/java/com/kh/matdori/restcontroller/BusinessRestController.java
+++ matdori/src/main/java/com/kh/matdori/restcontroller/BusinessRestController.java
@@ -1,5 +1,36 @@
 package com.kh.matdori.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.matdori.dao.BusinessDao;
+import com.kh.matdori.dto.BusinessDto;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/business")
 public class BusinessRestController {
+	
+	@Autowired
+	private BusinessDao businessDao;
+	
+	//사업자 가입
+	@PostMapping("/join")
+	public void insert(@RequestBody BusinessDto businessDto) {
+		businessDao.insert(businessDto);
+	}
+	
+//	//사업자 마이페이지 조회
+//	@GetMapping("/mypage/{busId}")
+//	public ResponseEntity<BusinessDto> find(@PathVariable String busId){
+//		
+//	}
 
 }
