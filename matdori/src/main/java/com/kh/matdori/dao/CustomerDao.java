11ad68d66b1d19dc3f08354bfcbabc38a221ba04
yudaeun03 <appae6772@naver.com>
2023-11-08 10:56:35 +0900

    CustomerDao, mapper 생성중

--- matdori/src/main/java/com/kh/matdori/dao/CustomerDao.java
+++ matdori/src/main/java/com/kh/matdori/dao/CustomerDao.java
@@ -1,11 +1,13 @@
 package com.kh.matdori.dao;
 
+import java.util.List;
+
 import com.kh.matdori.dto.CustomerDto;
 
 public interface CustomerDao {
 	void insert(CustomerDto customerDto);
-	boolean delete(String customerId);
-
-
-	
+//	boolean delete(String customerId);
+	boolean updateCustomerInfo(CustomerDto customerDto);
+	CustomerDto selectOne(String CustomerId);
+	List<CustomerDto> selectList();
 }
--- matdori/src/main/java/com/kh/matdori/dao/CustomerDaoImpl.java
+++ matdori/src/main/java/com/kh/matdori/dao/CustomerDaoImpl.java
@@ -1,5 +1,9 @@
 package com.kh.matdori.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.matdori.dto.CustomerDto;
@@ -8,16 +12,39 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Repository
-public class CustomerDaoImpl implements CustomerDao {@Override
+public class CustomerDaoImpl implements CustomerDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	
+	@Override
 	public void insert(CustomerDto customerDto) {
+		sqlSession.insert("customer.insert", customerDto);
+	}
+
+//	@Override
+//	public boolean delete(String customerId) {
+//		int result = sqlSession.delete("customer.delete", customerId);
+//		if(result == 0) throw new NoTargetException();
+//	}
+
+	@Override
+	public boolean updateCustomerInfo(CustomerDto customerDto) {
 		// TODO Auto-generated method stub
-		
+		return false;
 	}
 
 	@Override
-	public boolean delete(String customerId) {
+	public CustomerDto selectOne(String CustomerId) {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
+	}
+
+	@Override
+	public List<CustomerDto> selectList() {
+		return sqlSession.selectList("customer.selectAll");
 	}
 
 }
