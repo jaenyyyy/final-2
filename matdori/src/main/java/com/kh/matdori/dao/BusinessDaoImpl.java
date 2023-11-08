14ee7474915647113ef5820f1ded81cbbcd685f3
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-11-08 11:19:45 +0900

    1. Restaurant 폴더 생성(dao, dto, controller)

--- matdori/src/main/java/com/kh/matdori/dao/BusinessDaoImpl.java
+++ matdori/src/main/java/com/kh/matdori/dao/BusinessDaoImpl.java
@@ -1,8 +1,22 @@
 package com.kh.matdori.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.matdori.dto.BusinessDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Repository
 public class BusinessDaoImpl implements BusinessDao {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void insert(BusinessDto businessDto) {
+		sqlSession.insert("business.join", businessDto);
+	}
 }
