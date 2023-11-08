2091b88a81b6b34db5ceb766a69e5bba13e938a5
ph2tiger <ph2tiger@naver.com>
2023-11-08 13:31:50 +0900

    오타수정

--- matdori/src/main/java/com/kh/matdori/dto/BusinessDto.java
+++ matdori/src/main/java/com/kh/matdori/dto/BusinessDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 public class BusinessDto {
 	private String busId, busPw;
 	private String busRegNo;
-	private String busName, busTel, usEmail;
+	private String busName, busTel, busEmail;
 	private String busPost, busAddr1, busAddr2;
 	private Date busRegDate;
 
