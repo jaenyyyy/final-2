b793f02d465b1ec49211fa47223e0acf3cc2b02b
yudaeun03 <appae6772@naver.com>
2023-11-17 18:57:22 +0900

    pull request올립니당!!

---
+++ matdori/src/main/java/com/kh/matdori/dto/CustomerAdminListDto.java
@@ -0,0 +1,24 @@
+package com.kh.matdori.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class CustomerAdminListDto {
+	private String customerId;
+	private String customerPw;
+	private String customerName;
+	private String customerEmail;
+	private int customerContact;
+	private Date customerBirth;
+	private String customerGender;
+	private String customerLevel;
+	private int customerPoint;
+	private Date customerBlockTime;
+	private String customerBlockComment;
+	private String customerStatus;
+}
--- matdori/src/main/java/com/kh/matdori/dto/CustomerBlockDto.java
+++ matdori/src/main/java/com/kh/matdori/dto/CustomerBlockDto.java
@@ -10,9 +10,9 @@ import lombok.NoArgsConstructor;
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class CustomerBlockDto {
 	
+
 	private String customerId; // 이용자 아이디 
 	private String customerStatus; // 이용자 상태 (차단상태 Y / N)
 	private Date customerBlockTime; // 이용자 차단 시각 
 	private String customerBlockComment; // 이용자 차단 사유 
-
 }
