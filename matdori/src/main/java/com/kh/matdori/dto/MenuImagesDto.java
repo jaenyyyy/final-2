46c1295b7f91779af15afd61ddc5a0aa9efd549f
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-11-20 02:17:28 +0900

    이미지 삽입 작업중

---
+++ matdori/src/main/java/com/kh/matdori/dto/MenuImagesDto.java
@@ -0,0 +1,12 @@
+package com.kh.matdori.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class MenuImagesDto {
+	private int menuNo;
+    private int attachNo;
+}
