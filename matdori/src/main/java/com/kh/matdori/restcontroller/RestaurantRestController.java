395bf6cc1ea9415b7b5f05375652af417ad887f7
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-11-23 18:06:00 +0900

    레스토랑 이미지 삽입 구현중

--- matdori/src/main/java/com/kh/matdori/restcontroller/RestaurantRestController.java
+++ matdori/src/main/java/com/kh/matdori/restcontroller/RestaurantRestController.java
@@ -1,12 +1,17 @@
 package com.kh.matdori.restcontroller;
 
+import java.io.File;
 import java.net.URI;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,9 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.matdori.configuration.FileUploadProperties;
 import com.kh.matdori.dao.AdminDao;
 import com.kh.matdori.dao.RestaurantDao;
+import com.kh.matdori.dto.ResImagesDto;
 import com.kh.matdori.dto.RestaurantDto;
+import com.kh.matdori.vo.ResImagesVO;
 import com.kh.matdori.vo.RestaurantJudgeVO;
 
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +41,16 @@ public class RestaurantRestController {
 	@Autowired
 	private AdminDao adminDao;
 	
+	// 초기 디렉터리 설정
+		@Autowired
+		private FileUploadProperties props;
+		private File dir;
+
+		@PostConstruct
+		public void init() {
+			dir = new File(props.getHome());
+			dir.mkdirs();
+		}
 	@PostMapping("/")
 	public ResponseEntity<?> insert(@RequestBody RestaurantJudgeVO vo) {
 		 // 디버그 로그 출력
@@ -75,4 +93,21 @@ public class RestaurantRestController {
 		return restaurantDao.selectOne(resNo);
 	}
 
-}
+	 @PostMapping("/{resNo}/images")
+	    public ResponseEntity<Void> uploadImages(@PathVariable int resNo, @ModelAttribute ResImagesVO vo) {
+	        restaurantDao.insertResImage(resNo, vo.getResImages());
+	        return ResponseEntity.ok().build();
+	    }
+
+	    @GetMapping("/{resNo}/images")
+	    public ResponseEntity<List<ResImagesDto>> getImages(@PathVariable int resNo) {
+	        List<ResImagesDto> images = restaurantDao.selectResImagesByResNo(resNo);
+	        return ResponseEntity.ok(images);
+	    }
+
+	    @DeleteMapping("/image/{attachNo}")
+	    public ResponseEntity<Void> deleteImage(@PathVariable int attachNo) {
+	        boolean isDeleted = restaurantDao.deleteResImage(attachNo);
+	        return isDeleted ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+	    }
+	}
