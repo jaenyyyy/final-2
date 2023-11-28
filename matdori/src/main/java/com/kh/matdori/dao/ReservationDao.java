0afa96a408b07a2ba92f8957e5fc0187b51e412e
ph2tiger <ph2tiger@naver.com>
2023-11-28 14:20:48 +0900

    리뷰이미지 안뜨던거 오류수정

--- matdori/src/main/java/com/kh/matdori/dao/ReservationDao.java
+++ matdori/src/main/java/com/kh/matdori/dao/ReservationDao.java
@@ -9,23 +9,19 @@ import com.kh.matdori.vo.MenuWithImagesVO;
 
 import com.kh.matdori.dto.ReviewDto;
 
-
 public interface ReservationDao {
-   int sequence();
+	int sequence();
 
-   void insert(ReservationDto reservationDto);
-   
-   ReservationDto selectOne(int rezNo);  //결제상세
-   
-   boolean isInReservation(int rezResNo, int rezClockNo, int rezSeatNo);
-   
-   List<ReservationListDto> rezList(String rezCustomerId); //회원별 예약조회
-   
-   List<ReservationListDto> rezList(CusPaginationVO vo); //회원별 예약조
-   
+	void insert(ReservationDto reservationDto);
 
-   List<MenuWithImagesVO> menuList(int rezNo); //다수의 메뉴
+	ReservationDto selectOne(int rezNo); // 결제상세
 
+	boolean isInReservation(int rezResNo, int rezClockNo, int rezSeatNo);
 
+	List<ReservationListDto> rezList(CusPaginationVO vo); // 회원별 예약조회리스트
+
+	int rezCount(CusPaginationVO vo);//페이지네이션?
+	
+	List<MenuWithImagesVO> menuList(int rezNo); // 다수의 메뉴
 
 }
