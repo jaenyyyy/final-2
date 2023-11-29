6892c6134429c530a7071b5cec65479468d4f382
TEDDDDGHCHCVBBCB <akdrjsdl102@naver.com>
2023-11-29 16:53:21 +0900

    매장 심사전 화면 안나오기 구현중

--- matdori/src/main/java/com/kh/matdori/restcontroller/AdminRestController.java
+++ matdori/src/main/java/com/kh/matdori/restcontroller/AdminRestController.java
@@ -1,7 +1,9 @@
 package com.kh.matdori.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,48 +21,48 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/rest/admin")
 public class AdminRestController {
-	
+
 	@Autowired
 	private AdminDao adminDao;
-	
-	
-	//레스토랑 차단 기능
-    @RequestMapping("/restaurant/block")
-    public String resBlock(@RequestBody RestaurantBlockDto restaurantBlockDto) {
-    	adminDao.insertResBlock(restaurantBlockDto);
-    	return "redirect:/admin/restaurant/detail";
-    }
-    
-    
-    //레스토랑 차단 해제
-    @RequestMapping("/restaurant/cancel")
-    public String resCancel(@RequestBody RestaurantBlockDto restaurantBlockDto) {
-    	adminDao.deleteResBlock(restaurantBlockDto.getResNo());
-    	return "redirect:/admin/restaurant/detail";
-    }
-    
-    
-    //레스토랑 심사 수정 (승인 / 거절로 업데이트)
-    @RequestMapping("/restaurant/judge")
-    public void updateJudge(@RequestBody RestaurantJudgeDto restaurantJudgeDto) {
-    	adminDao.updateResJudge(restaurantJudgeDto);
-    }
-    
-    
- // 해시태그 추가 기능
-    @PostMapping("/hashtag/insert")
-    public void insert(@ModelAttribute HashtagDto hashtagDto) {
-    	int hashNo = adminDao.hashSequence();
-    	hashtagDto.setHashNo(hashNo);
-    	adminDao.insertHashtag(hashtagDto);
-    }
-    
-    //해시태그 삭제 기능
-    @PostMapping("/hashtag/delete")
-    public void delete(@RequestParam int hashNo) {
-    	adminDao.deleteHashtag(hashNo);
-    	
-    }
-    
-    
+
+	// 레스토랑 차단 기능
+	@RequestMapping("/restaurant/block")
+	public String resBlock(@RequestBody RestaurantBlockDto restaurantBlockDto) {
+		adminDao.insertResBlock(restaurantBlockDto);
+		return "redirect:/admin/restaurant/detail";
+	}
+
+	// 레스토랑 차단 해제
+	@RequestMapping("/restaurant/cancel")
+	public String resCancel(@RequestBody RestaurantBlockDto restaurantBlockDto) {
+		adminDao.deleteResBlock(restaurantBlockDto.getResNo());
+		return "redirect:/admin/restaurant/detail";
+	}
+
+	// 레스토랑 심사 수정 (승인 / 거절로 업데이트)
+	@RequestMapping("/restaurant/judge")
+	public void updateJudge(@RequestBody RestaurantJudgeDto restaurantJudgeDto) {
+		adminDao.updateResJudge(restaurantJudgeDto);
+	}
+
+	// 해시태그 추가 기능
+	@PostMapping("/hashtag/insert")
+	public void insert(@ModelAttribute HashtagDto hashtagDto) {
+		int hashNo = adminDao.hashSequence();
+		hashtagDto.setHashNo(hashNo);
+		adminDao.insertHashtag(hashtagDto);
+	}
+
+	// 해시태그 삭제 기능
+	@PostMapping("/hashtag/delete")
+	public void delete(@RequestParam int hashNo) {
+		adminDao.deleteHashtag(hashNo);
+
+	}
+
+	@GetMapping("/restaurant/judge/{resNo}")
+		public RestaurantJudgeDto find(@PathVariable int resNo) {
+			return adminDao.selectOne(resNo);
+		}
+
 }
